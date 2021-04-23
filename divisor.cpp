497a06172c8aa954fcbf4cd775705bfb86ddca31
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-04-23 12:16:33 +0200

    Update divisor.cpp

--- divisor.cpp
+++ divisor.cpp
@@ -1,1 +1,13 @@
+#include "include" //
+using namespcae std; 
 
+int main()
+{
+  for(int i = 1000; i < 10000; i ++){
+    
+    if(i%17 && i %137)
+    {
+      std::cout << i std::endl; 
+    }
+  }
+}
